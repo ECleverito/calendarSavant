3fb3adedc7fa27e4050366a453f47c3ff4e1c3e9
Erich Clever <erichclever@Erichs-MacBook-Air.local>
2018-10-30 07:42:47 -0400

    Add buffer year to remove nonexistent array element access error
    Remove test cases and realized that 0 corresponds to Sunday in function for obtaining weekday number

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(){
 	
 	//A 3D array of three years of weeks is created where each day in a week 
 	//holds an 8-byte character array that indicates the date
-	char arrayOfWeeks[YEARS*WEEKS_PER_YEAR][DAYS_IN_WEEK][DATE_REP_LENGTH];
+	char arrayOfWeeks[(YEARS+1)*WEEKS_PER_YEAR][DAYS_IN_WEEK][DATE_REP_LENGTH];
 					
 	//Store current date from system in char array
 	char currentDate[9]; //Needs extra byte at end to store null terminator
@@ -47,27 +47,17 @@ int main(){
 	bool leapYearFlag;
 	int daysInThisMonth;
 	
-	//Char arrays for converting year, month, and day ints
-	char currentYearChars[4],
-		 currentMonthChars[2],
-		   currentDayChars[2];
-	
 	int y = currentYearInt;
 	int m = currentMonthInt;
 	int d = currentDayInt;
 	
 	int weekNumber = 0;
 	
-	intToCharArray(y,4,currentYearChars);
-	std::cout << std::endl;
-	std::cout.put(currentYearChars[3]);
-	std::cout << std::endl;
-	
 	for(;y <= (currentYearInt + YEARS);y++){
 
 		//Checking if year is a leap year
 		(y%4) ? (leapYearFlag = 0) : (leapYearFlag = 1);
-		
+
 		//Reset the month counter
 		if(m==13)
 			m=1;
@@ -81,11 +71,13 @@ int main(){
 				//Special case for February with leap year
 				if(m==2)
 					(leapYearFlag) ? (daysInThisMonth = 29) : (daysInThisMonth = 28);
+				else{
 
-				if(m%2)
-					daysInThisMonth = 31;
-				else
-					daysInThisMonth = 30;
+					if(m%2)
+						daysInThisMonth = 31;
+					else
+						daysInThisMonth = 30;
+				}
 
 			}
 			else{
@@ -104,8 +96,8 @@ int main(){
 				intToCharArray(y,4,&arrayOfWeeks[weekNumber][wdInt][4]);
 
 				wdInt++;
-				
-				//Reset after Sunday (wdInt=6)
+
+				//Reset after Saturday (wdInt=6). Sun = 0.
 				if(wdInt==7)
 				{
 					weekNumber++;
@@ -113,7 +105,7 @@ int main(){
 				}
 
 			}
-			
+
 			//Reset day counter after exiting loop
 			d=1;
 			m++;
@@ -122,10 +114,12 @@ int main(){
 
 	}
 
-	// for(int n = 0;n<8;n++){
-	// 	std::cout << std::endl;
-	// 	std::cout.put(arrayOfWeeks[6][4][n]);
-	// }
+	//Testing array access
+	for(int n = 0;n<8;n++){
+		std::cout << std::endl;
+		std::cout.put(arrayOfWeeks[2][4][n]);
+	}
+	std::cout << std::endl;
 	
 	return 0;
 }
